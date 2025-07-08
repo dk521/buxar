92483f757d398931ea111729d74419e8ea0d59cc
= <=>
2025-07-08 20:47:35 +0530

    2nd changes

--- create_update.py
+++ create_update.py
@@ -8,7 +8,14 @@ class Update:
 
 
 # git add steps
+# git clone 
 # git status
 # 1. git add - 10 files, want to send only 2 files. -> to stage the changes
-# 2. git commit 
-# 3. git push
+# 2. git commit - stamp: final changes I want to make to server. message (staged changes)
+# 3. git push - (commited changes)
+
+
+# Excercise 
+# 1. create a new repository on github. 
+# 2. git clone 
+# 3. 3 changes push with commit message. 
