658a85a0b6313e3de5f78bb77f7f1ecd26ecce55
= <=>
2025-07-08 20:41:20 +0530

    I have completed, create and update class for DB

---
+++ create_update.py
@@ -0,0 +1,14 @@
+
+class Create:
+    pass
+
+class Update:
+    pass
+
+
+
+# git add steps
+# git status
+# 1. git add - 10 files, want to send only 2 files. -> to stage the changes
+# 2. git commit 
+# 3. git push
